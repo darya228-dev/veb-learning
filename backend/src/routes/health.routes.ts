1325181467b2ac7182f1e807d522e942ba2f5087
unknown <milukovadara@knu.ua>
2026-04-01 19:44:27 +0300

    0.2.0

--- backend/src/routes/health.routes.ts
+++ backend/src/routes/health.routes.ts
@@ -1,8 +1,10 @@
-import * as controller from "../controllers/health.controller";
 import { Router } from "express";
+import * as controller from "../controllers/health.controller";
 import wrap from "../infrastructure/wrap";
 
 const router = Router();
+
+// GET /api/v1/health
 router.get("/health", wrap(controller.health));
 
 export default router;
